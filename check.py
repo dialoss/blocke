b146b18b6c63203abdf7a42d7f9a7f0a61017066
Alex <dialoss775@gmail.com>
2024-10-24 11:07:01 +0400

    1

--- check.py
+++ check.py
@@ -16,10 +16,6 @@ nest_asyncio.apply()
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-
-# ФИКС МЕДЛЕЕЕЫЕ ЗАПРОСЫ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 async def html_to_pdf(html_content):
     browser = await launch(handleSIGINT=False,
                            handleSIGTERM=False,
@@ -62,7 +58,6 @@ async def html_to_pdf(html_content):
 
 
 def get_check(bank, d):
-    settings = sessions[bank].query(Settings).first()
     data = {
                'DOCUMENT': '4252228616',
                'SENDER_CARD': '2314',
@@ -70,9 +65,8 @@ def get_check(bank, d):
                'CODE': '212251',
                'AMOUNT': '1234.13',
                'TIME': datetime.now().strftime('%d.%m.%Y %H:%M'),
-               # 'SENDER_NAME': settings.fio,
                'RECEIVER_NAME': 'Скам Скамыч Ф.',
-               'RECEIVER_NUMBER': '+7 (993) 532-49-05',
+               'RECEIVER_NUMBER': '+7 (993) 111-22-33',
            } | d
     with open(os.path.join(script_dir, bank, '1.html'), 'r', encoding='utf-8') as file:
         html_content = file.read()
@@ -81,7 +75,32 @@ def get_check(bank, d):
     html_content = html_content.replace('$URL', 'http://127.0.0.1:5000/' + bank)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(html_to_pdf(html_content))
-    return open(os.path.join(script_dir, 'check.pdf'), 'rb').read()
+    
+    from PyPDF2 import PdfReader, PdfWriter
+    from datetime import datetime
+
+    pdf_path = os.path.join(script_dir, 'check.pdf')
+    reader = PdfReader(pdf_path)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    writer.add_metadata({
+        '/Title': 'Квитанция-11100399',
+        '/Author': '',
+        '/Subject': '"/reports/IB/Receipt"',
+        '/Keywords': '22.10.2024 08:41:12|f8848b21-135a-4ad6-b014-15c00b30e714|7258',
+        '/Producer': 'OpenPDF 1.3.30.jaspersoft.2',
+        '/Creator': 'JasperReports Library version 6.20.3-415f9428cffdb6805c6f85bbb29ebaf18813a2ab',
+        '/CreationDate': datetime(2024, 10, 22, 8, 41, 12),
+        '/ModDate': datetime(2024, 10, 22, 8, 41, 12)
+    })
+
+    with open(pdf_path, 'wb') as output_file:
+        writer.write(output_file)
+
+    return open(pdf_path, 'rb').read()
 
 
-get_check('tinkoff', {'SENDER_NAME': 'Егор Иванович Ж.', 'RECEIVER_NAME': 'Алексей Михайлович З.', "AMOUNT": '59664.10'})
+# get_check('tinkoff', {'SENDER_NAME': 'Егор Иванович Ж.', 'RECEIVER_NAME': 'Алексей Михайлович З.', "AMOUNT": '59664.10'})
